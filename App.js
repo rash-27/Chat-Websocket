85f47873eec67e608da5e74c840c382012977239
Rashmik <rashmik2705@gmail.com>
2024-01-14 20:02:10 +0530

    Add a socket on connection and update clients-total

--- App.js
+++ App.js
@@ -3,9 +3,26 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+let socketConnected = new Set();
+
+const io = require('socket.io')(server);
+
+io.on('connection',onConnected)
+
+function onConnected(socket){
+    console.log(`New connection: ${socket.id}`)
+    socketConnected.add(socket.id)
+    io.emit('clients-total',socketConnected.size)
+
+    socket.on('disconnect',()=>{
+        console.log(`Disconnected: ${socket.id}`)
+        socketConnected.delete(socket.id)
+        io.emit('clients-total',socketConnected.size)
+    })
+}
---
+++ public/main.js
@@ -0,0 +1,8 @@
+const socket = io();
+
+const clientsTotal = document.getElementById('clients-total');
+
+socket.on('clients-total', (data) => {
+    clientsTotal.innerText = `Total clients: ${data}`;
+});
+
